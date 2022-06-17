b209bd3d20b1b54bc4586e58887b8ec9d50f1f35
Eleonore Stultjens <eleonore.jens@gmail.com>
2022-06-17 16:46:30 +0200

    Ex 1 of Operateurs

---
+++ Operateur_Ex_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Operateur_Ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+
+            int division = a / b;
+            Console.WriteLine(division);
+            int modulo = a % b;
+            Console.WriteLine(modulo);
+            double precis = (double)a / b;
+            Console.WriteLine(precis);
+
+        }
+    }
+}
