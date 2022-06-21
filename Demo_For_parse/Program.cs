4b528d98b44387c545e0ab207a374940f32a6572
Eleonore Stultjens <eleonore.jens@gmail.com>
2022-06-21 15:57:41 +0200

    demo for non arithmétique

---
+++ Demo_For_parse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Demo_For_parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Boucle for
+
+            #region Arithmétique
+            for (int i = 0; i <= 10; i+=2)
+            {
+                Console.ForegroundColor = (ConsoleColor)i;
+                Console.WriteLine(i);
+            }
+            //i n'existe que dans le contexte de la boucle For: Console.WriteLine(i);
+            #endregion
+
+            #region Non Arithmétique
+            Console.WriteLine("Veuillez indiquer un numéro: ");
+            bool isOk = true;
+            //tant que ce n'est pas un numéro alors je redemande un input, si ce n'est pas un numéro, je rentre dans ma boucle et je peux prévenir l'utilisateur: j'ai deux conditions il faut que ca ne soit pas un numéro et que l'user ait dit "oui" donc le isOk est true.
+            for (string input = Console.ReadLine(); !int.TryParse(input, out _) && isOk; input = Console.ReadLine())
+            {
+                Console.WriteLine("Sais-tu ce qu'est un numéro? (Oui/Non)");
+                isOk = Console.ReadLine() == "Oui";
+            }
+            Console.WriteLine("Vous avez entré un nombre correct");
+            #endregion
+
+            #endregion
+
+        }
+    }
+}
