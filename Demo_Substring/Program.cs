ee851d8e275809b351e37c50c997cee0f69f7e65
Eleonore Stultjens <eleonore.jens@gmail.com>
2022-06-21 14:55:16 +0200

    substring, traingle, bban et iban exercices

---
+++ Demo_Substring/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo_Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+            Console.WriteLine(message.Substring(0, 5));
+            Console.WriteLine(message.Substring(message.Length - 6, 6));
+            Console.WriteLine(message.Substring(6, 6));
+        }
+    }
+}
--- Operateur_Ex_1/Program.cs
+++ Operateur_Ex_1/Program.cs
@@ -6,16 +6,25 @@ namespace Operateur_Ex_1
     {
         static void Main(string[] args)
         {
-            int a = 5;
-            int b = 2;
-
-            int division = a / b;
-            Console.WriteLine(division);
-            int modulo = a % b;
-            Console.WriteLine(modulo);
-            double precis = (double)a / b;
-            Console.WriteLine(precis);
-
+            Console.WriteLine("Veuillez indiquer un premier nombre: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int a))
+            {
+                Console.WriteLine("Veuillez introduire un second nombre: ");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out int b) && b != 0)
+                {
+                    //int division = a / b;
+                    //Console.WriteLine(division);
+                    //int modulo = a % b;
+                    //Console.WriteLine(modulo);
+                    //double precis = (double)a / b;
+                    //Console.WriteLine(precis);
+                    Console.WriteLine($"Division entière : {a / b} \t Modulo : {a % b} \t Division : {(double)a/b}");
+                }
+                else Console.WriteLine($"Le nombre {input} n'est pas valide");
+            }
+            else Console.WriteLine($"Le nombre {input} n'est pas valide");
         }
     }
 }
