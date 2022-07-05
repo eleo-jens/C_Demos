241f1a0057c45353b049f22931967e304dbccaaf
Eleonore Stultjens <eleonore.jens@gmail.com>
2022-07-05 15:02:10 +0200

    Demo Structure

---
+++ Demo_Structures/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Demo_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Bienvenu sur la borne automatique de l'{Euromillion.GAME_NAME}");
+
+            Euromillion combinaison;
+            Console.WriteLine("Veuillez encoder vos chiffres.");
+            Console.WriteLine("Premier chiffre: ");
+            combinaison.code1 = int.Parse(Console.ReadLine()); // Vaut mieux faire un tryparse 
+            Console.WriteLine("Deuxième chiffre: ");
+            combinaison.code2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Troisième chiffre: ");
+            combinaison.code3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quatrième chiffre: ");
+            combinaison.code4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Cinquième chiffre: ");
+            combinaison.code5 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Veuillez encoder vos étoiles.");
+            Console.WriteLine("Première étoile: ");
+            combinaison.star1 = short.Parse(Console.ReadLine());
+            Console.WriteLine("Deuxième étoile: ");
+            combinaison.star2 = short.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Votre combinaison est : {combinaison.code1} - {combinaison.code2} - {combinaison.code3} - {combinaison.code4} - {combinaison.code5} *{combinaison.star1} *{combinaison.star2}");
+
+            // ici pour utiliser une structure avec les valeurs par défaut (à la va vite) utiliser un new
+            // création d'une instance: préparer une zone mémoire pour pouvoir stocker les valeurs par défaut
+            // si on ne crée par d'instance il faut obligatoirement initialiser toutes les variables constituant la structure
+            // new = opérateur
+            Euromillion combinaison_gagnante = new Euromillion();
+
+            Console.WriteLine($"Votre combinaison est : {combinaison_gagnante.code1} - {combinaison_gagnante.code2} - {combinaison_gagnante.code3} - {combinaison_gagnante.code4} - {combinaison_gagnante.code5} *{combinaison_gagnante.star1} *{combinaison_gagnante.star2}");
+
+        }
+    }
+}
---
+++ Demo_Structures/Structs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// meme namespace que notre projet
+namespace Demo_Structures
+{
+    public struct Euromillion
+    {
+        public const string GAME_NAME = "EUROMILLION";
+        public int code1, code2, code3, code4, code5;
+        public short star1, star2;
+    }
+}
